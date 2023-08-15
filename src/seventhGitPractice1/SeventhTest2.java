f3cb771b3afc7327f71fd934c62cab4971142f77
mreshta <mreshta10@gmail.com>
2023-08-15 12:18:42 -0700

    The second class

--- src/seventhGitPractice1/SeventhTest2.java
+++ src/seventhGitPractice1/SeventhTest2.java
@@ -5,6 +5,11 @@ public class SeventhTest2 {
 	public static void main(String[] args) {
 		System.out.println("This is the 7th class");
 		System.out.println("This is the 7th class");
+		System.out.println("This is the 7th class");
+		System.out.println("This is the 7th class");
+		System.out.println("This is the 7th class");
+		System.out.println("This is the 7th class");
+		System.out.println("This is the 7th class");
 
 	}
 
