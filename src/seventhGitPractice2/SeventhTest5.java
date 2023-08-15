bde4788c4252b5eb2d1c7f4c2fecbba27f23fd6f
mreshta <mreshta10@gmail.com>
2023-08-15 14:41:14 -0700

    Alien to the practice 2 test5

---
+++ src/seventhGitPractice2/SeventhTest5.java
@@ -0,0 +1,14 @@
+package seventhGitPractice2;
+
+public class SeventhTest5 {
+
+	public static void main(String[] args) {
+		System.out.println("This is the third class");
+		System.out.println("This is the third class");
+		System.out.println("This is the third class");
+		System.out.println("This is the third class");
+		System.out.println("This is the third class");
+
+	}
+
+}
