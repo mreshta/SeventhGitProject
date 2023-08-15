cb9b235799385176f26743a1e9dc253753a37aa5
mreshta <mreshta10@gmail.com>
2023-08-15 12:15:56 -0700

    The second class

---
+++ src/seventhGitPractice1/SeventhTest2.java
@@ -0,0 +1,11 @@
+package seventhGitPractice1;
+
+public class SeventhTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is the 7th class");
+		System.out.println("This is the 7th class");
+
+	}
+
+}
