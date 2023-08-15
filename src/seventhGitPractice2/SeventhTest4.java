ed9521f7f06880af7f6e95cb309923f58deceabe
mreshta <mreshta10@gmail.com>
2023-08-15 14:00:41 -0700

    The practice 2 test 4

---
+++ src/seventhGitPractice2/SeventhTest4.java
@@ -0,0 +1,13 @@
+package seventhGitPractice2;
+
+public class SeventhTest4 {
+
+	public static void main(String[] args) {
+		System.out.println("This is the third class");
+		System.out.println("This is the third class");
+		System.out.println("This is the third class");
+		System.out.println("This is the third class");
+
+	}
+
+}
